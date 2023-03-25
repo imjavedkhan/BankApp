d77c5bf1ae774dc09fe37676fb60f3ddd96fe49a
Javed Khan <javedalikhan50@gmail.com>
2023-03-25 21:23:41 +0530

    spring security with in-memory user details

---
+++ src/main/java/com/primus/BankApp/SecurityConfig/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.primus.BankApp.SecurityConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests()
+                .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+                .requestMatchers("/notices","/contact").permitAll()
+                .and().formLogin().and().httpBasic();
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+}
---
+++ src/main/java/com/primus/BankApp/controller/AccountController.java
@@ -0,0 +1,13 @@
+package com.primus.BankApp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    @RequestMapping("/myAccount")
+    public String getAccountDetails(){
+        return "Here are the account details from the DB";
+    }
+}
